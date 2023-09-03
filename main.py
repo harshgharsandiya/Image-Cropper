668a79a9fd56f23e8a503f7453c77be6337c0b3a
Harsh Gharsandiya <114327999+harshgharsandiya@users.noreply.github.com>
2023-09-03 18:18:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,25 @@ def crop_to_coordinates(input_image_path, output_image_path, left, top, right, b
     # Save the cropped image to the output path
     cropped_image.save(output_image_path)
 
+input_path= input("Enter path of Input Images Folder: ")
+output_path= input("Enter path of Output Images Folder: ")
+
 left = 10  # Replace with the left edge coordinate
 top = 380  # Replace with the top edge coordinate
 right = 400 # Replace with the right edge coordinate
 bottom = 1039-200  # Replace with the bottom edge coordinate
 
+for i in range(1,6):
+    input_image_path= input_path+str(i)+".png"
+    output_image_path= output_path+str(i)+"(i).png"
+    crop_to_coordinates(input_image_path, output_image_path, left, top, right, bottom)
+  
+left = 790  # Replace with the left edge coordinate
+top = 50  # Replace with the top edge coordinate
+right = 1920 # Replace with the right edge coordinate
+bottom = 1039-600  # Replace with the bottom edge coordinate
 
 for i in range(1,6):
-    input_image_path= str(i)+".png"
-    output_image_path= str(i)+"(i).png"
+    input_image_path= input_path+str(i)+".png"
+    output_image_path= output_path+str(i)+"(ii).png"
     crop_to_coordinates(input_image_path, output_image_path, left, top, right, bottom)
